c4477d9029bde5765cea1ff2c55ae87c2105a800
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2025-02-04 17:22:59 -0300

    :bug: Update scraper to capture product titles from new class and limit results to 49

--- catalogo/scraper.py
+++ catalogo/scraper.py
@@ -4,10 +4,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 
 url = "https://lista.mercadolivre.com.br/computador-gamer-i7-16gb-ssd-1tb"
 options = Options()
@@ -18,23 +16,24 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 driver.get(url)
 sleep(3)
 
-#search_box = driver.find_element(By.ID, "cb1-edit")
-#search_box.send_keys("Computador Gamer i7 16gb ssd 1tb")
-#search_box.send_keys(Keys.RETURN)
-
+# Esperar que os elementos dos produtos estejam carregados
 WebDriverWait(driver, 10).until(
-    EC.presence_of_all_elements_located((By.CLASS_NAME, "poly-component__title"))
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "andes-card"))
 )
 
-# Capturar os nomes dos produtos e os links
-product_elements = driver.find_elements(By.CLASS_NAME, "poly-component__title")
+# Capturar todos os elementos que representam os produtos
+product_elements = driver.find_elements(By.CLASS_NAME, "andes-card")
 
-count = 1
-# Exibir os resultados
-for product in product_elements:
-    name = product.text  # Nome do produto
-    link = product.get_attribute("href")  # Link do produto
-    print(f"Produto: {name}\nLink: {link}\n")
-    count += 1
-    print(count)
+for index, product in enumerate(product_elements, start=1):
+    if index > 49:
+        break
+    
+    try:
+        # Localizar o título do produto dentro da div do produto usando CLASS_NAME
+        title_element = product.find_element(By.CLASS_NAME, "poly-component__title")
+        title = title_element.text.strip()
+        print(f"Produto {index}: {title}")
+    except Exception as e:
+        print(f"Erro ao capturar o título do produto {index}: {e}")
+
 driver.quit()
