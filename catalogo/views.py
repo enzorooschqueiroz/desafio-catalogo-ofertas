7c29ad4582f3d46a22eb064b4df89ac9ea7739e9
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2025-02-05 21:35:33 -0300

    :sparkles: Add product listing page with table layout and view function

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from catalogo.models import Produto
 
+def list_products(request):
+    produtos = Produto.objects.all()
+    return render(request, "catalogo/list_products.html", {"produtos": produtos})
 # Create your views here.
--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from catalogo.views import list_products
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", list_products, name = "list_products")
 ]
